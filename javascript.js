af237307a95a2132c31f0773944a9cfcdb4d4056
KSM <ij2356@m-sync.co.kr>
2023-02-24 12:59:18 +0900

    비동기

--- javascript.js
+++ javascript.js
@@ -1,38 +1,55 @@
 
-	class UserStorage{
-		loginUser(id, password, onSuccess, onError) {
-			setTimeout(() => {
-				if (id === "superAdmin" && password === "1234") {
-          onSuccess(id);
-        } else {
-          onError(new Error("not found"));
-        }
-			}, 2000);
-		}
-		getRoles(user, onSuccess, onError) {
-			setTimeout(() => {
-				if (user === 'superAdmin') {
-					onSuccess({ name: "superAdmin", role: "admin" });
-				}
-				else {
-					onError(new Error("no Acess"))
-				}
+	// class UserStorage{
+	// 	loginUser(id, password, onSuccess, onError) {
+	// 		setTimeout(() => {
+	// 			if (id === "superAdmin" && password === "1234") {
+  //         onSuccess(id);
+  //       } else {
+  //         onError(new Error("not found"));
+  //       }
+	// 		}, 2000);
+	// 	}
+	// 	getRoles(user, onSuccess, onError) {
+	// 		setTimeout(() => {
+	// 			if (user === 'superAdmin') {
+	// 				onSuccess({ name: "superAdmin", role: "admin" });
+	// 			}
+	// 			else {
+	// 				onError(new Error("no Acess"))
+	// 			}
 					
-			}, 1000);
-		}
+	// 		}, 1000);
+	// 	}
 
-	}
+	// }
 
 
-	const userStorge = new UserStorage();
-	const id = prompt("enter your id")
-	const password = prompt('enter yout password')
-	userStorge.loginUser(id, password, (user) => {
-		userStorge.getRoles(user, userWithRole => {
-			alert(`Hello ${userWithRole.name} you have a ${userWithRole.role} role`);
-		}, error => { console.log(error) });
-		error =>console.log(error);	
-	}
-	)
+	// const userStorge = new UserStorage();
+	// const id = prompt("enter your id")
+	// const password = prompt('enter yout password')
+	// userStorge.loginUser(id, password, (user) => {
+	// 	userStorge.getRoles(user,
+	// 		userWithRole => {
+	// 		alert(`Hello ${userWithRole.name} you have a ${userWithRole.role} role`);
+	// 		},
+	// 		error => { console.log(error) });
+	// 	error =>console.log(error);	
+	// }
+	// )
 	
 
+
+	function delayP(sec) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(new Date().toISOString());
+        }, sec * 1000)
+    })
+}
+
+async function myAsync() {
+    await delayP(3).then((time) => console.log(time));	//2022-02-07T05:09:49.460Z
+    return 'async';
+}
+
+myAsync().then((result) => console.log(result));    //'async'
